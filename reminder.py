76d4fe5c7894b0b916464a459b4fb46b03ac09d2
Neeraja Menon <rneerajamenon@gmail.com>
2019-03-05 17:19:34 +0530

    tested add and view reminder

--- reminder.py
+++ reminder.py
@@ -4,29 +4,60 @@ import sqlite3
 from functools import wraps
 
 DATABASE = 'reminder.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to log in first.')
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    status_code = 200
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+            request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try again.'
+            status_code = 401
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error), status_code
+
 @app.route('/add', methods=['POST'])
+@login_required
 def add():
     task = request.form['task']
     reminder = request.form['reminder']
-    if not task or not reminder:
+    if not task or not reminder: 
         flash("All fields are required. Please try again.")
         return redirect(url_for('main'))
     else:
         g.db = connect_db()
-        g.db.execute('insert into Reminder(task,reminder) values (?, ?)',[request.form['task'], request.form['reminder']])
+        g.db.execute('insert into Reminder(Task,Task_Description) values (?, ?)',[request.form['task'], request.form['reminder']])
         g.db.commit()
         g.db.close()
         flash('New entry was successfully posted!')
         return redirect(url_for('main'))
 
-def login():
-    session['logged_in'] = True
-    return redirect(url_for('main'))
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 @app.route('/main')
 @login_required
@@ -36,6 +67,6 @@ def main():
     reminders = [dict(task=row[0], reminder=row[1]) for row in cur.fetchall()]
     g.db.close()
     return render_template('main.html', reminders=reminders)
-    #return "Reminder App"
+
 if __name__ == '__main__':
     app.run(debug=True)
